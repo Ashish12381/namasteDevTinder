74d7aed05db5f90ff01b12da6a573a1936a4da85
Ashish <apal74180@gmail.com>
2026-06-10 08:30:00 +0530

    routing,dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
-app.use("/hello", (req, res) => {
-  res.send("Hello World from /hello route");
-});
-app.use("/", (req, res) => {
-  res.send("Hello World from root route");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("Hello World from /hello route");
+// });
+app.get("/user/:userId/:name",(req,res)=>{
+    // console.log(req.query)
+    console.log(req.params)
+    res.send({"firstName":"Ashis","lastName":'Pal'})
+})
+app.post("/user",(req,res)=>{
+    res.send("User created successfully")
+})
+app.delete("/user",(req,res)=>{
+    res.send("User deleted successfully")
+})
+// app.use("/", (req, res) => {
+//   res.send("Hello World from root route");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
