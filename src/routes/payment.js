4defb1e97e7e430cfeeb71849f70243f48d61ede
Ashish <apal74180@gmail.com>
2026-07-03 00:44:52 +0530

    payment verify

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -83,5 +83,18 @@ paymentRouter.post("/webhook", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+paymentRouter.get("/payment/verify", userAuth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    return res.status(200).json({
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
 
 module.exports = paymentRouter;
